e4e26f4281107c9af632e8a9bc80fae6e80ef87b
whitegoogles <jsdjava@gmail.com>
2017-07-23 13:09:47 -0500

    Still working on CORS

--- app.js
+++ app.js
@@ -12,22 +12,14 @@ const nameLimit = 20;
 const roomLimit = 36;
 
 var allowCrossDomain = function(req, res, next) {
+	console.log("OK LETS ALLOW FOR CORS PLEASE REEEE");
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+    res.header('Access-Control-Allow-Headers', 'Origin,Content-Type, Authorization, Content-Length, X-Requested-With');
+	next();
 };
 
-app.configure(function(){
-	app.use(allowCrossDomain);
-});
+app.use(allowCrossDomain);
 
 // Put all API endpoints under '/api'
 app.get('/room', (req, res) => {
